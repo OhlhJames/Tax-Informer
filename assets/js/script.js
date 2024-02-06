20a440605629422ada37e62a3fb2685716ff40d0
IsItIzzet <ohlhjames@gmail.com>
2024-02-05 20:59:47 -0600

    Fixed functionality of searchbar so that users can search for desired URLs and have their information be console logged and if their zipcode is redacted or missing for any other reason, an error code will be console logged instead of tax information.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@
 var urlName = document.querySelector('#search-url');
 var zipcode = document.querySelector('#zip-code'); 
 var searchButton = document.querySelector('#search-button');
-searchButton.addEventListener('click',grabTax)
-var grabTax = function(){
-    console.log("button click successful")
-    fetch('https://api.api-ninjas.com/v1/urllookup?url=' + urlName, {
+
+var grabTax = function(event){
+    event.preventDefault();
+    console.log("button click successful");
+    fetch('https://api.api-ninjas.com/v1/urllookup?url=' + urlName.value, {
         method: 'GET',
         headers: {
         'X-Api-Key': 'q4Zaxex+vW0+7EKi63fEIw==wDXzY9E7RwxLv6Ub',
@@ -18,22 +19,27 @@ var grabTax = function(){
             console.log(data)
             companyZip = data.zip;
             console.log(companyZip)
-        fetch('https://api.api-ninjas.com/v1/salestax?zip_code=' + companyZip, {
-            method: 'GET',
-            headers: {
-            'X-Api-Key': 'q4Zaxex+vW0+7EKi63fEIw==wDXzY9E7RwxLv6Ub',
-        },
-        })
-            .then(function (response) {
-            return response.json();
-        })
-            .then(function (data2) {
-            console.log(data2)
-            totalTaxRate = data2[0].total_rate;
-            console.log(totalTaxRate);
+            if(companyZip != ""){ 
+                fetch('https://api.api-ninjas.com/v1/salestax?zip_code=' + companyZip, {
+                    method: 'GET',
+                    headers: {
+                    'X-Api-Key': 'q4Zaxex+vW0+7EKi63fEIw==wDXzY9E7RwxLv6Ub',
+                    }
+                })
+                    .then(function (response) {
+                    return response.json();
+                })
+                    .then(function (data2) {
+                    console.log(data2)
+                    totalTaxRate = data2[0].total_rate;
+                    console.log(totalTaxRate);
+                });
+            }else{
+                console.log("error finding zip code, redacted for privacy")
+            }
         });
-});
 }
+searchButton.addEventListener('click',grabTax)
 
 /* if(company.tax > .09 ){
   company.tax.color= red
