3224f0cd61d5bb409179fcc5c37cb76653774ce6
IsItIzzet <ohlhjames@gmail.com>
2024-02-05 20:25:05 -0600

    adjust html to be interactive with javascript so that user input could be taken and read by the APIs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,39 @@
 
-var urlName = 'google.com' /*document.querySelector('#url-name'); */
+var urlName = document.querySelector('#search-url');
 var zipcode = document.querySelector('#zip-code'); 
-
-fetch('https://api.api-ninjas.com/v1/urllookup?url=' + urlName, {
-    method: 'GET',
-    headers: {
-      'X-Api-Key': 'GFxmJh+HYnvd21whXHNOdg==BrQLzDzNqIDIS3uU',
-    },
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data)
-    companyZip = data.zip;
-    console.log(companyZip)
-    fetch('https://api.api-ninjas.com/v1/salestax?zip_code=' + companyZip, {
-      method: 'GET',
-      headers: {
-        'X-Api-Key': 'GFxmJh+HYnvd21whXHNOdg==BrQLzDzNqIDIS3uU',
-    },
+var searchButton = document.querySelector('#search-button');
+searchButton.addEventListener('click',grabTax)
+var grabTax = function(){
+    console.log("button click successful")
+    fetch('https://api.api-ninjas.com/v1/urllookup?url=' + urlName, {
+        method: 'GET',
+        headers: {
+        'X-Api-Key': 'q4Zaxex+vW0+7EKi63fEIw==wDXzY9E7RwxLv6Ub',
+        },
     })
-    .then(function (response) {
-      return response.json();
+        .then(function (response) {
+            return response.json();
     })
-    .then(function (data2) {
-      console.log(data2)
-      totalTaxRate = data2[0].total_rate;
-      console.log(totalTaxRate);
-    });
+        .then(function (data) {
+            console.log(data)
+            companyZip = data.zip;
+            console.log(companyZip)
+        fetch('https://api.api-ninjas.com/v1/salestax?zip_code=' + companyZip, {
+            method: 'GET',
+            headers: {
+            'X-Api-Key': 'q4Zaxex+vW0+7EKi63fEIw==wDXzY9E7RwxLv6Ub',
+        },
+        })
+            .then(function (response) {
+            return response.json();
+        })
+            .then(function (data2) {
+            console.log(data2)
+            totalTaxRate = data2[0].total_rate;
+            console.log(totalTaxRate);
+        });
 });
+}
 
 /* if(company.tax > .09 ){
   company.tax.color= red
